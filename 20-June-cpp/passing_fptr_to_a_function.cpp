32e2b42976478a9831a387f39a706c58148a4c81
Mamillapalli-Susmitha-ML <Mamillapalli.Susmitha@maplelabs.com>
2023-06-23 17:19:14 +0530

    Updated

--- 20-June-cpp/passing_fptr_to_a_function.cpp
+++ 20-June-cpp/passing_fptr_to_a_function.cpp
@@ -1,29 +1,22 @@
 #include<iostream>
-#include <iterator>
 #include<vector>
-
 using namespace std;
 void printvalue(int);
 typedef void(*fptr)(int);
-
+void foreach(fptr fp,int a);
 int main(){
     
     vector <int> values={1,2,3,4,5};
     fptr fp1= printvalue;
     for(vector<int>::iterator it=values.begin();it<values.end();it++)
     {
-       // cout << "Value " << *it << endl;
-        fp1(*it);
+        foreach(fp1,*it);
     }
-    
-
 }
 void printvalue(int a)
 {
     std::cout<<"value="<<a<<std::endl;
 }
-
-//foreach function not tested it seems, pls check once.
 void foreach(fptr fp,int a)
 {
     fp(a);
